dc0d73835b1752555ed9d7e9da7d41e380cd530f
Haider Muhammad Abbas <oca2602it0024@edu.oca.ac.jp>
2026-08-28 05:35:48 +0000

    Initial commit (Rails setup)

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,46 @@
+class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new, status: :unprocessable_entity
+   end
+  end
+ 
+ def show
+     @product = Product.find(params[:id])
+ end
+
+ def index
+    @products = Product.all
+ end
+
+ def edit
+    @product = Product.find(params[:id])
+ end
+
+  def update
+    @product = Product.find(params[:id])
+     if @product.update(product_params)
+       redirect_to product_path # 商品詳細にリダイレクト
+     else
+       render :edit  # 失敗時に編集画面に戻る
+     end
+   end
+
+def destroy
+     @product = Product.find(params[:id])
+     @product.destroy
+     redirect_to products_path 
+   end
+
+ private
+  def product_params
+   params.require(:product).permit(:name, :description, :price)
+  end
+end
